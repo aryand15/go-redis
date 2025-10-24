89022c106b81463fae911c751c62ca107fb37075
Aryan Deshpande <aryandeshpande15@gmail.com>
2025-10-24 16:02:17 -0400

    hardcoded PONG response

--- app/main.go
+++ app/main.go
@@ -8,17 +8,26 @@ import (
 
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
+	// Create a TCP connection, listening on port 6379.
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	// Block until another host attempts connection with the listener.
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	// Attempt to send PONG by default when a connection is received.
+	pong := []byte("+PONG\r\n")
+	_, err = conn.Write(pong)
+	if err != nil {
+		fmt.Println("Error sending PONG response: ", err.Error())
 		os.Exit(1)
 	}
 }
